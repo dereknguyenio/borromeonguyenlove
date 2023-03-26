3270243f97a243494a520a746487df4068e54443
dereknguyenio <derek_nguyen@live.com>
2023-03-26 11:54:33 -0500

    rsvp test

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbwJEASozsn-KW4MhSGmXG4mSzXrEe95atMnVxYsPEilkTjVKXE473MiDLQs0Yeg2-E/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbyEtvpcJ0-S7ydYAiIefLOUBFQox5A1XTwmlQ4IGBKdjCBaTqSyDrHrKYAG39EiGeYA/exec', data)
                 .done(function (data) {
                     console.log(data);
                     if (data.result === "error") {
